7a53793d8ebfec0fba49939c97ee5290094da548
ayushkr1701 <ayushkr1701@gmail.com>
2022-12-29 13:54:56 +0530

    added build.sh

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Install the dependencies for the project
+pip install -r requirements.txt
+
+# Run Django's collectstatic command to gather all static files
+python manage.py collectstatic --noinput
+
+# Run Django's migrate command to apply any outstanding database migrations
+python manage.py migrate
+
+# Start the Django development server
+python manage.py runserver 
