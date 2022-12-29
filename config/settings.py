71e03d7f51bf26b190f5b90bd81a7f51147ce929
ayushkr1701 <ayushkr1701@gmail.com>
2022-12-30 01:15:34 +0530

    bool

--- config/settings.py
+++ config/settings.py
@@ -152,7 +152,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ALLOWED_ORIGINS = ['*']
-
+# CORS_ALLOWED_ORIGINS = ['*']
+CORS_ALLOW_ALL_ORIGINS: True
 
 FILE_UPLOAD_PERMISSIONS=0o640
