a4485397196b648fae8afa5dd2097acdfa88333f
ayushkr1701 <ayushkr1701@gmail.com>
2022-12-29 17:59:29 +0530

    superuser

--- build.sh
+++ build.sh
@@ -9,6 +9,8 @@ python manage.py collectstatic --noinput
 # Run Django's migrate command to apply any outstanding database migrations
 python manage.py migrate
 
+python manage.py createsuperuser --noinput --username ayush --email ayushkr1701@gmail.com --password Abhay@123
+
 # Start the Django development server
 # gunicorn config.wsgi
 
