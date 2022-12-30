2232b2e9cf1875f111b394fbb610b67014d7b250
ayushkr1701 <ayushkr1701@gmail.com>
2022-12-30 11:49:39 +0530

    staticroot

--- config/settings.py
+++ config/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-kngepvxt6e582&9*-xvhcln0uo!k*^%tm_@5uw42inc-h@6$lh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -141,11 +141,11 @@ USE_TZ = True
 
 # MEDIA_ROOT is for the user-uploaded content
 
-STATIC_URL = '/opt/render/project/src/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
+STATIC_ROOT = '/opt/render/project/src/static/'
 
-MEDIA_URL = '/opt/render/project/src/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = '/opt/render/project/src/media/'
 
 
 # Default primary key field type
